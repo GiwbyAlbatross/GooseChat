18e00b91d149c524511ab5b4bc3c6a40a1dabf58
Giwby Albatross <giwby.albatross@gmail.com>
2025-05-22 04:22:53 +0000

    Attempt to satisfy linter complaints

--- goosechat/entry.py
+++ goosechat/entry.py
@@ -6,11 +6,13 @@ import os
 ENTRIES_FILE = os.environ.get('GOOSECHAT_ENTRIES_FILE', './chatlog.txt')
 
 class Entry:
+    "class representing an entry in the chat log"
     seperator: str='/'
     timestamp: float
     user: str
     msg: str
     def dump(self) -> str:
+        "dump this Entry into a string to put in a chat log file"
         return self.seperator.join(
             [
                 str(self.timestamp),
@@ -20,23 +22,29 @@ class Entry:
         )
     @classmethod
     def load(cls, d: str):
+        "create an Entry from a string generated from Entry.dump"
         return cls(*(d.split(cls.seperator, 2)))
     def __init__(self, timestamp: str|float, user: str, msg: str):
-        if isinstance(timestamp, str): self.timestamp = float(timestamp)
-        else: self.timestamp = timestamp
+        if isinstance(timestamp, str):
+            self.timestamp = float(timestamp)
+        else:
+            self.timestamp = timestamp
         self.user = user
         self.msg  = msg
     def __repr__(self) -> str:
         return f"Entry(timestamp={self.timestamp!r}, user={self.user!r}, msg={self.msg!r})"
     def __eq__(self, other) -> bool:
-        return self.timestamp == other.timestamp and self.user == other.user and self.msg == other.msg
+        return self.timestamp == other.timestamp and \
+            self.user == other.user and self.msg == other.msg
 
 def add_msg(msg: str, user: str='guest', timestamp: Optional[float]=None):
+    "add a message into the global chat log"
     if timestamp is None:
         timestamp = time.time()
     entry = Entry(timestamp, user, msg)
-    with open(ENTRIES_FILE, 'a') as f:
+    with open(ENTRIES_FILE, 'a', encoding='utf-8') as f:
         f.write(entry.dump())
 def add_entry(entry: Entry):
-    with open(ENTRIES_FILE, 'a') as f:
+    "add an Entry into the global chat log"
+    with open(ENTRIES_FILE, 'a', encoding='utf-8') as f:
         f.write(entry.dump())
--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env -S python3 -m pytest
+"Test the GooseChat web application backend. "
 from goosechat import entry
 
 def test_entry_load_dump():
+    "test if the Entry.load and Entry.dump functions are working..."
     e = entry.Entry(739294728402847481.4828472,
                     'reallyAmazingUsername2014',
                     'I am a person with a really amazing username. I have a / in my message!')
