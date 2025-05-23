11d22537b4565ca9df3afd3699e77d5d069604f8
Giwby Albatross <giwby.albatross@gmail.com>
2025-05-23 11:00:19 +1000

    Add some more template html and some time conversion script stuff

---
+++ static/time_conversion.js
@@ -0,0 +1,20 @@
+function getLang() {
+    // credit to Fiach Reid for writing this
+    if (navigator.languages !== undefined) 
+        return navigator.languages[0]; 
+    return navigator.language;
+}
+
+const dayOfWeekTranslator = new Intl.DateTimeFormat(getLang(), {'weekday':'long'});
+
+function humanTime(seconds) {
+    const time = new Date(seconds*1000);
+    const minutesAgo = ((Date.now().getTime() / 1000) - seconds)/60;
+    if (minutesAgo < 16) {
+        return minutesAgo.toString() + " minutes ago";
+    }
+    if (minutesAgo < 24*60*6) {
+        return dayOfWeekTranslator.format(time) + ' ' + time.getHours()+':'+time.getMinutes();
+    }
+    return time.toString();
+}
